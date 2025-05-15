3abf610746ee3f284b76d8fdcbc23c23ea65f26a
Wagner Araújo <wagnercorp17@gmail.com>
2025-05-15 20:12:55 -0300

    First comit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Todos from './Todos/todos'
+
+
+function App() {
+  return (
+    <>
+      <h1>Todo List App</h1>
+      <div>
+        <Todos />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Todos/todos.tsx
@@ -0,0 +1,7 @@
+export default function Todos(){
+  return(
+    <>
+    <h2>Completed Todos</h2>
+    </>
+  )
+}
